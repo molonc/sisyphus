45578adc84ee9af8004d6fce5b0cca136a1e8af5
Andrew McPherson <andrew.mcpherson@gmail.com>
2018-11-06 17:11:03 -0800

    bugfix: get url fixed

--- dbclients/tantalus.py
+++ dbclients/tantalus.py
@@ -43,15 +43,15 @@ class BlobStorageClient(object):
 
     def get_url(self, blobname):
         sas_token = self.blob_service.generate_blob_shared_access_signature(
-            self.storage_account,
+            self.storage_container,
             blobname,
             permission=azure.storage.blob.BlobPermissions.READ,
-            expiry=datetime.datetime.utcnow() + datetime.timedelta(hours=1),
+            expiry=datetime.datetime.utcnow() + datetime.timedelta(hours=12),
         )
         blob_url = self.blob_service.make_blob_url(
             container_name=self.storage_container,
             blob_name=blobname,
-            protocol="http",
+            protocol="https",
             sas_token=sas_token,
         )
         return blob_url
