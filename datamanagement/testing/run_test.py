88dcede1b7080df40ecbbc46033b6c90cea1377e
Sophia Chan <sochan@bccrc.ca>
2018-11-28 22:22:19 +0000

    feature: check that output bams and results exist on Tantalus and Azure

--- datamanagement/testing/run_test.py
+++ datamanagement/testing/run_test.py
@@ -14,16 +14,24 @@ LIBRARY_ID = "A96213ATEST"
 
 test_dataset = tantalus_api.get("sequence_dataset", library__library_id=LIBRARY_ID)
 
-# TODO: set up a celery worker locally on VM 
 
 def download_data(storage_name, storage_dir, queue_name, tag_name):
-
-	# TODO: check this get or create
+	"""
+	Get the storage storage_name from Tantalus (create it if it doesn't 
+	already exist) and download input fastqs tagged with tag_name from 
+	singlecellblob to storage storage_name using Saltant.
+	Args:
+		storage_name: name of storage to download files to
+		storage_dir: directory corresponding to storage_name
+		queue_name: celery queue to use
+		tag_name: tag in Tantalus that identifies the input fastqs
+	"""
 	tantalus_api.get_or_create(
-		"storage", 
+		"storage_server", 
 		storage_type="server", 
 		name=storage_name,
-		storage_directory=storage_dir)
+		storage_directory=storage_dir,
+		queue_prefix=storage_name)
 
 	file_transfer_args = dict(
 		tag_name=tag_name,
@@ -39,6 +47,10 @@ def download_data(storage_name, storage_dir, queue_name, tag_name):
 
 
 def run_pipeline(pipeline_version, storage_name):
+	"""
+	Create a test user configuration file and use it to 
+	run the pipeline using Sisyphus.
+	"""
 	config_dir = os.path.join(os.path.dirname(workflows.__file__), "config")
 	test_config_path = os.path.join(config_dir, "test_config.json")
 	test_config = file_utils.load_json(os.path.join(config_dir, "normal_config.json"))
@@ -62,12 +74,47 @@ def run_pipeline(pipeline_version, storage_name):
 	subprocess.check_call(run_cmd)
 
 
-def check_bams(align_analysis, hmmcopy_analysis):
-	# TODO: determine storage client based on whether we're running on Azure or on Shahlab
-	headnode_client = tantalus_api.get_storage_client("headnode")
-	data_client = tantalus_api.get_storage_client("singlecellblob")
+def check_outputs(file_paths, storage_name):
+	"""
+	Check to see whether the filepaths exist at storage_name and 
+	have been added to Tantalus.
+	"""
+	file_resources = set()
 
-	# Get the bam paths from the inputs yaml on the headnode
+	storage_client = tantalus_api.get_storage_client(storage_name)
+	for file_path in file_paths:
+		filename = os.path.relpath(file_path, storage_client.prefix)
+
+		if not client.exists(filename):
+			raise Exception("filename {} could not be found in storage {}".format(filename, storage_name))
+
+		try:
+			file_resource = tantalus_api.get("file_resource", filename=filename)
+		except NotFoundError:
+			raise Exception("no file resource with filename {} exists".format(filename))
+
+		file_resources.add(file_resource["id"])
+
+		file_instance_exists = False
+		for file_instance in file_resource["file_instances"]:
+			if file_instance["storage"]["name"] == storage_name:
+				file_instance_exists = True
+
+		if not file_instance_exists:
+			raise Exception("no file instance in {} exists for file resource {}".format(
+				storage_name, file_resource["id"]))
+
+	return file_resources
+
+
+def check_bams(align_analysis, hmmcopy_analysis):
+	align_analysis = tantalus_api.get("analysis", name=JIRA_TICKET + "_align")
+	hmmcopy_analysis = tantalus_api.get("analysis", name=JIRA_TICKET + "_hmmcopy")
+
+	# TODO: determine storage client based on whether we're running on Azure or on Shahlab
+
+	# Get bam paths from the inputs yaml
+	headnode_client = tantalus_api.get_storage_client("headnode")
 	assert len(align_analysis["logs"]) == 1
 	inputs_yaml_pk = analysis["logs"][0]
 	f = headnode_client.open_file(tantalus_api.get("file_resource", id=inputs_yaml_pk)["filename"])
@@ -76,84 +123,34 @@ def check_bams(align_analysis, hmmcopy_analysis):
 		bam_paths.append(info["bam"])
 	f.close()
 
-	bam_file_resources = set()
-
-	for bam_path in bam_paths:
-		filename = os.path.relpath(bam_path, data_client.prefix)
-		if not data_client.exists(filename):
-			raise Exception("output bam {} could not be found in singlecellblob".format(filename))
-
-		try:
-			file_resource = tantalus_api.get("file_resource", filename=filename)
-		except NotFoundError:
-			raise Exception("no file resource with filename {} exists".format(filename))
-
-		if not file_resource["analysis"] == analysis["id"]:
-			raise Exception("analysis field in file resource {} does not match {}".format(
-				file_resource["id"], align_analysis["name"]))
-
-		bam_file_resources.add(file_resource["id"])
-
-		file_instance_exists = False
-		for file_instance in file_resource["file_instances"]:
-			if file_instance["storage"]["name"] == "singlecellblob":
-				file_instance_exists = True
-
-		if not file_instance_exists:
-			raise Exception("no file instance in singlecellblob exists for file resource {}".format(
-				file_resource["id"]))
-
+	bam_file_resources = check_outputs(bam_paths, "singlecellblob")
 	dataset_file_resources = set()
 	for dataset_id in hmmcopy_analysis["input_datasets"]:
 		dataset = tantalus_api.get("sequence_dataset", id=dataset_id)
 		dataset_file_resources.update(dataset["file_resources"])
 
-	if file_resources != dataset_file_resources:
+	if bam_file_resources != dataset_file_resources:
 		raise Exception("file resources for output bams do not match hmmcopy input datasets")
 
 
-# def __is_gzip(self, filename):
-#    """
-#    Uses the file contents to check if the file is gzip or not.
-#    The magic number for gzip is 1f 8b
-#    """
-#    with open(filename) as f:
-#        file_start = f.read(4)
+def check_results():
+	for analysis_type in ("align", "hmmcopy"):
+		if analysis_type == "align":
+			dirname = "alignment"
+		else:
+			dirname = "hmmcopy_autoploidy"
 
-#        if file_start.startswith("\x1f\x8b\x08"):
-#            return True
-#        return False
+		info_yaml_path = os.path.join(JIRA_TICKET, "results", "results", dirname, "info.yaml")
+		f = results_client.open_file(info_yaml_path)
+		results_infos = ["results"].values()
+		f.close()
 
-# def get_file_linecount(self):
-#     def blocks(files, size=65536):
-#         while True:
-#             b = files.read(size)
-#             if not b: break
-#             yield b
+		result_paths = [result_info["filename"] for result_info in result_infos]
+		result_file_resources = check_outputs(result_paths, "singlecellblob_results")
 
-#     if self.__is_gzip(self.args.input):
-#         input_stream = gzip.open(self.args.input, 'rb')
-#     else:
-#         input_stream = open(self.args.input)
-
-#     return sum(bl.count("\n") for bl in blocks(input_stream))
-
-def check_outputs():
-	# TODO: check the results have been pushed tantalus and uploaded to azure with the api
-	# check number of cells in each table, 
-
-	results_client = tantalus_api.get_storage_client("singlecellblob_results")
-
-	align_analysis = tantalus_api.get("analysis", name=JIRA_TICKET + "_align")
-	hmmcopy_analysis = tantalus_api.get("analysis", name=JIRA_TICKET + "_hmmcopy")
-
-	check_bams(align_analysis, hmmcopy_analysis)
-
-	align_results = tantalus_api.get("results", JIRA_TICKET + )
-
-	# TODO: load the info yaml and make sure all the files exist on Azure and are in Tantalus
-
-	pass
+		result = tantalus_api.get("results", name=JIRA_TICKET + "_" + analysis_type)
+		if result_file_resources != set(result["file_resources"]):
+			raise Exception("file resources for result files do not match those in {}".format(result["name"]))
 
 
 def cleanup():
@@ -175,3 +172,5 @@ if __name__ == '__main__':
 	tag_name = "IntegrationTestFastqs"
 	download_data(args["storage_name"], args["storage_dir"], args["queue_name"], tag_name)
 	run_pipeline(args["pipeline_version"], args["storage_name"])
+	check_bams()
+	check_results()
