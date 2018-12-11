8e37d37763a673ecf7ae9d41ea4df96c5de5854f
amcpherson <andrew.mcpherson@gmail.com>
2018-12-10 22:20:54 -0800

    Update dlp.py

--- datamanagement/utils/dlp.py
+++ datamanagement/utils/dlp.py
@@ -199,7 +199,6 @@ def create_sequence_dataset_models(
             log.info("creating sequence dataset {}".format(sequence_dataset["name"]))
             dataset = tantalus_api.get_or_create("sequence_dataset", **sequence_dataset)
 
-        # Add tag and post dataset to tag endpoint
         if tag_name is not None:
             tantalus_api.tag(tag_name, dataset['id']) 
 
