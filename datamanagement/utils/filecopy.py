058a43f883a4b03d5ad37e872374819ec7162785
Mike Yue <36213563+Mike-Yue@users.noreply.github.com>
2018-12-10 22:11:05 -0800

    SISY-86: gzip file check

--- datamanagement/utils/filecopy.py
+++ datamanagement/utils/filecopy.py
@@ -44,3 +44,27 @@ def rsync_file(from_path, to_path):
 
     if os.path.getsize(to_path) != os.path.getsize(from_path):
         log.error("copy failed for %s to %s", from_path, to_path)
+
+
+def try_gzip(path):
+    subprocess_cmd = [
+        "gzip",
+        "-t",
+        path,
+    ]
+
+    log.info(" ".join(subprocess_cmd))
+
+    # The following is a way to use the logging module with subprocess.
+    # See
+    # https://stackoverflow.com/questions/21953835/run-subprocess-and-print-output-to-logging.
+    process = Popen(subprocess_cmd, stdout=PIPE, stderr=STDOUT)
+
+    with process.stdout:
+        for line in iter(process.stdout.readline, b""):
+            log.info(line)
+
+    exitcode = process.wait()
+
+    if exitcode != 0:
+        raise Exception("cmd '{}' returned {}".format(" ".join(subprocess_cmd), exitcode))
