fbf00e938bf63885b4aeafcae2bae14950a9673b
Simong Song <ssong@node0515.(none)>
2019-01-17 09:45:36 -0800

    changes added to script

--- datamanagement/tantalus_filepath_check.py
+++ datamanagement/tantalus_filepath_check.py
@@ -1,119 +1,97 @@
-import sys
 import os
-import paramiko
-import logging
-import json
+import sys
 from dbclients.tantalus import TantalusApi
-import argparse
-from dbclients.basicclient import NotFoundError
-from datamanagement.utils.runtime_args import parse_runtime_args
-import pandas as pd
-from sets import Set
+import click
 
-id_to_test = [1, 3, 4, 6, 9, 10, 14]
+@click.command()
+@click.option('--name', type=str)
+def init(name):
+    print "STARTING"
 
-
-if __name__ == '__main__':
-
-    assert len(sys.argv) > 3,  "PLEASE SET A VALID HOSTNAME, USERNAME AND PASSWORD\n eg. to test on rocks, the script should run as:\n python manage.py 10.9.4.27 user123 password123  "
-    ssh = paramiko.SSHClient()
-
-    ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-    try:
-        ssh.connect(str(sys.argv[1]), username=str(sys.argv[2]), password=str(sys.argv[3]))
-        sftp = ssh.open_sftp()
-    except paramiko.SSHException:
-        print("Connection Error, Please check your hostname")
-
-    tantalus_api  = TantalusApi()
-    tantalus_filecheck_result = open("tantalus_filecheck_result.txt", "w+")
-    blob_storage = tantalus_api.get_storage_client('singlecellblob')
+    tantalus_api = TantalusApi()
+    tantalus_filecheck_result = open("tantalus_filepath_check_result.txt", "w+")
+    # blosb_torage = tantalus_api.get_storage_client('singlecellblob')
     fail_flag = False
 
+    print "Collecting File Resources..."
 
-    #Non Azure
-    storage_to_test_server = []
-    #Azure
-    storage_to_test_blob = []
+    file_resources = tantalus_api.list('file_resource', fileinstance__storage__name=name)
 
-    print "Initializing on hostname: " + sys.argv[1]
+    print "File Resources collected..."
 
-    for id in id_to_test:
-        storage = tantalus_api.get("storage", id=id)
-        if storage['storage_type'] ==  "server":
-            storage_to_test_server.append(storage)
-        elif storage['storage_type'] == 'blob':
-            storage_to_test_blob.append(storage)
-        else:
-            pass
+    assert file_resources, "Empty File Resources is empty"
 
-    count = 0
-    print "Collecting File Instances..."
+    blob_storage = tantalus_api.get_storage_client(name)
 
-    for storage in storage_to_test_server:
-        file_instances = tantalus_api.list('file_instance', storage__name=storage['name'])
+    print "Blob Storage collected..."
 
-        assert \
-            file_instances , \
-            storage['name'] + " is empty"
+    for file_resource in file_resources:
+        for file_instance in file_resource["file_instances"]:
+            if file_instance['storage']['name'] ==  name:
+                print "Checking " + file_instance['filepath'] + "... \n"
+                print file_resource["created"]
+                if blob_storage.exists(file_instance['filepath']):
+                    if  blob_storage.get_size(file_instance["filepath"]) != file_resource["size"] :
+                        print "Expected size do not match the actual size!"
+                        tantalus_filecheck_result.write(
+                            "ERROR: Size mismatch in: " + file_instance['filepath'] +
+                            "\nActual Size: " + str(os.path.getsize(file_instance["filepath"])) +
+                            "\nExpected Size: " + str(file_resource["size"]) + "\n"
+                        )
+                        tantalus_filecheck_result.write(file_resource["created"])
+                        tantalus_filecheck_result.write(blob_storage.get_created_time(file_instance["filepath"]))
+                        tantalus_filecheck_result.write("\n")
+                        fail_flag = True
 
-        for file_instance in file_instances:
-            print "Checking filepath: " + str(file_instance['filepath'])
-            try:
-                file_stat = sftp.stat(file_instance['filepath'])
-                print str(file_stat.st_size)
-                print str(tantalus_api.get('file_resource', id=file_instance['file_resource'])['size'])
-                if not file_stat.st_size == \
-                       tantalus_api.get('file_resource', id=file_instance['file_resource'])['size']:
-                    print "Expected size do not match the actual size!"
-                    tantalus_filecheck_result.write("ERROR: Size mismatch in: " + file_instance['filepath'] + \
-                                                    "\nActual Size: " + str(file_stat.st_size)  + \
-                                                    "\nExpected Size: " + str(tantalus_api.get('file_resource', id=file_instance['file_resource'])['size']) + "\n")
-                    fail_flag = True
-
+                    else:
+                        print "Passed"
                 else:
-                    print "Passed"
-
-            except IOError:
-                print "Invalid filepath!"
-                tantalus_filecheck_result.write("ERROR: " + file_instance['filepath'] + " is not a valid filepath \n")
-                fail_flag = True
-                continue
-
-    ssh.close()
-
-    for storage in storage_to_test_blob:
-        file_instances = tantalus_api.list('file_instance', storage__name=storage['name'])
-
-        assert \
-            file_instances, \
-            storage['name'] + " is empty"
-
-        for file_instance in file_instances:
-            print "Checking filepath: " + str(file_instance['filepath'])
-            file_resource = tantalus_api.get('file_resource', id=file_instance['file_resource'])
-            if not blob_storage.exists(file_resource['filename']):
-                print "Invalid filepath!"
-                tantalus_filecheck_result.write("ERROR: " + file_instance['filepath'] + " is not a valid filepath \n")
-                fail_flag = True
-
+                    print "file path does not exist or not valid"
+                    tantalus_filecheck_result.write("ERROR: " + file_instance['filepath'] + " is not a valid filepath \n")
+                    fail_flag = True
             else:
-                print str(file_resource['size'])
-                print str(blob_storage.get_size(file_resource['filename']))
-                if not blob_storage.get_size(file_resource['filename']) == file_resource['size']:
-                    print "Expected size do not match the actual size!"
-                    tantalus_filecheck_result.write("ERROR: Size mismatch in: " + file_instance['filepath'] + \
-                                                    "\nActual Size: " + str(blob_storage.get_size(file_resource['filename'])) + \
-                                                    "\nExpected Size: " + str(file_resource['size']) + "\n")
-                    fail_flag = True
-
-                else:
-                    print "Passed"
+                pass
 
 
+    # tantalus_filecheck_result.write("\n\n\n ---------- AZURE --------- \n\n\n")
+    #
+    # print "Testing filepath on Azure now"
+    # for storage in storage_to_test_blob:
+    #     file_instances = tantalus_api.list('file_instance', storage__name=storage['name'])
+    #
+    #     assert file_instances, storage['name'] + " is empty"
+    #
+    #     for file_instance in file_instances:
+    #         if "singlecelldata/data" in file_instance['filepath']:
+    #             print "Checking filepath: " + str(file_instance['filepath'])
+    #             file_resource = tantalus_api.get('file_resource', id=file_instance['file_resource'])
+    #             if not blob_storage.exists(file_resource['filename']):
+    #                 print "Invalid filepath!"
+    #                 tantalus_filecheck_result.write("ERROR: " + file_instance['filepath'] + " is not a valid filepath \n")
+    #                 fail_flag = True
+    #
+    #             else:
+    #                 print str(file_resource['size'])
+    #                 print str(blob_storage.get_size(file_resource['filename']))
+    #                 if not blob_storage.get_size(file_resource['filename']) == file_resource['size']:
+    #                     print "Expected size do not match the actual size!"
+    #                     tantalus_filecheck_result.write("ERROR: Size mismatch in: " +
+    #                                                     file_instance['filepath'] +  "\nActual Size: " +
+    #                                                     str(blob_storage.get_size(file_resource['filename'])) +
+    #                                                     "\nExpected Size: " + str(file_resource['size']) + "\n")
+    #                     fail_flag = True
+    #
+    #                 else:
+    #                     print "Passed"
+    #
 
     tantalus_filecheck_result.close()
 
+    print "TEST" + "FAILED! PLEASE CHECK THE tantalus_filepath_check_result.txt" if fail_flag else "PASSED!"
 
-    print "TEST" + "FAILED! PLEASE CHECK THE tantalus_filecheck_result.txt" if fail_flag else "PASSED!"
+
+if __name__ == '__main__':
+    init()
+
+
 
