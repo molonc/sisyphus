4e49f210bb735ebd3d4ae317562242695bbdd565
Simong Song <ssong@node0515.(none)>
2019-01-17 16:47:43 -0800

    refactored

--- datamanagement/testing/tantalus_filepath_check.py
+++ datamanagement/testing/tantalus_filepath_check.py
@@ -8,7 +8,6 @@ def init():
 
     tantalus_api = TantalusApi()
     tantalus_filecheck_result = open("tantalus_filepath_check_result.txt", "w+")
-    file_instances = {}
     fail_flag = False
 
     print "Collecting File Resources..."
@@ -25,48 +24,46 @@ def init():
     print "Blob Storage collected..."
 
     for file_resource in file_resources:
+        file_instances = {}
         for file_instance in file_resource["file_instances"]:
             file_instances[file_instance['storage']['name']] = file_instance
 
-            if file_instance['storage']['name'] ==  "shahlab":
-                print "Checking " + file_instance['filepath'] + "... \n"
+        print "Checking " + file_instances['shahlab']['filepath']
+        if shahlabclient.exists(file_instances['shahlab']['filepath']):
+            if  shahlabclient.get_size(file_instances['shahlab']["filepath"]) != file_resource["size"] :
+                print "Expected size do not match the actual size!"
 
-                if shahlabclient.exists(file_instance['filepath']):
-                    if  shahlabclient.get_size(file_instance["filepath"]) != file_resource["size"] :
-                        print "Expected size do not match the actual size!"
+                tantalus_filecheck_result.write(
+                    "\n ERROR: Size mismatch in: " + file_instances['shahlab']['filepath'] +
+                    "\nActual Size: " + str(shahlabclient.get_size(file_instances['shahlab']['filepath'])) +
+                    "\nExpected Size: " + str(file_resource["size"]) + "\n"
+                )
 
-                        tantalus_filecheck_result.write(
-                            "\n ERROR: Size mismatch in: " + file_instance['filepath'] +
-                            "\nActual Size: " + str(shahlabclient.getsize(file_instance["filepath"])) +
-                            "\nExpected Size: " + str(file_resource["size"]) + "\n"
-                        )
+                tantalus_filecheck_result.write("Tantalus: ")
+                tantalus_filecheck_result.write(file_resource["created"])
+                tantalus_filecheck_result.write("\nShahlab: ")
+                tantalus_filecheck_result.write(shahlabclient.get_created_time(file_instances['shahlab']['filepath']))
 
-                        tantalus_filecheck_result.write("Tantalus: ")
-                        tantalus_filecheck_result.write(file_resource["created"])
-                        tantalus_filecheck_result.write("\nShahlab: ")
-                        tantalus_filecheck_result.write(shahlabclient.get_created_time(file_instances['shahlab']['filepath']))
+                if 'singlecellblob' in file_instances.keys() and singlecellblob.exists(
+                        file_instances['singlecellblob']['filepath']):
+                    tantalus_filecheck_result.write("\nSinglecellBlob: ")
+                    tantalus_filecheck_result.write(singlecellblob.get_created_time(file_instances['singlecellblob']['filepath']))
 
-                        if 'singlecellblob' in file_instances.keys() and singlecellblob.exists(
-                                file_instances['singlecellblob']['filepath']):
-                            tantalus_filecheck_result.write("\nSinglecellBlob: ")
-                            tantalus_filecheck_result.write(singlecellblob.get_created_time(file_instances['singlecellblob']['filepath']))
+                tantalus_filecheck_result.write("\n")
+                fail_flag = True
 
-                        tantalus_filecheck_result.write("\n")
-                        fail_flag = True
-
-                    else:
-                        print "Passed"
-                else:
-                    print "file path does not exist or not valid"
-                    tantalus_filecheck_result.write("DELETED: " + file_instance['filepath'] + " is not a valid filepath \n")
-                    ail_flag = True
-                    try:
-                        tantalus_api.delete("file_instance", file_instance["id"])
-                    except:
-                        print "Deletion failed. Ignoring"
-                        continue
             else:
-                pass
+                print "Passed"
+        else:
+            print "file path does not exist or not valid"
+            tantalus_filecheck_result.write("DELETED: " + file_instances['shahlab']['filepath'] + " is not a valid filepath \n")
+            fail_flag = True
+            try:
+               tantalus_api.delete("file_instance", file_instances['shahlab']["id"])
+            except:
+                print "Deletion failed. Ignoring"
+                continue
+
 
 
     tantalus_filecheck_result.close()
