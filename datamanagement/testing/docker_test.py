bb1f3e3a0f2a9f279cada15ab98724f5b025f2dd
Simong Song <ssong@ubuntu.db05mufnxksuxnvyhl1tyrwfed.ux.internal.cloudapp.net>
2019-01-30 21:05:50 +0000

    docker

--- datamanagement/testing/docker_test.py
+++ datamanagement/testing/docker_test.py
@@ -4,12 +4,10 @@ import time
 
 if __name__ == '__main__':
     print "TANTALUS CREATING..."
-    time.sleep(5)
     tantalus_api = TantalusApi()
-    time.sleep(5)
     print "COLOSSUS CREATING..."
-    time.sleep(5)
     colossus_api = ColossusApi()
-    time.sleep(5)
+    console.log("HELLO")
+
 
 
