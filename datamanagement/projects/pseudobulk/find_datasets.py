5f552fd7c44fc0557611def77b9a5fc8da3ad3c5
Andrew McPherson <andrew.mcpherson@gmail.com>
2019-02-13 14:01:25 -0800

    datamanagement: organize pseudobulk

--- datamanagement/projects/pseudobulk/find_datasets.py
+++ datamanagement/projects/pseudobulk/find_datasets.py
@@ -6,6 +6,7 @@ import os
 import sys
 import logging
 import click
+import json
 import pprint
 
 import pandas as pd
@@ -16,43 +17,46 @@ from dbclients.tantalus import TantalusApi
 from dbclients.basicclient import NotFoundError
 
 
-@click.command()
-@click.argument('sample_ids_filename', required=True)
-@click.argument('tag_name', required=True)
-def find_pseudobulk_datasets(sample_ids_filename, tag_name):
-    sample_ids = [l.strip() for l in open(sample_ids_filename).readlines()]
+def get_dataset(tantalus_api, sample_id, library_id):
+    datasets = list(tantalus_api.list(
+        'sequence_dataset',
+        sample__sample_id=sample_id,
+        library__library_id=library_id,
+        library__library_type__name='SC_WGS',
+        dataset_type='BAM',
+        aligner__name='BWA_ALN_0_5_7',
+        reference_genome__name='HG19',
+    ))
 
+    datasets = filter(lambda d: d['is_complete'], datasets)
+
+    if len(datasets) != 1:
+        for dataset in datasets:
+            dataset['file_resources'] = '...'
+            print(json.dumps(dataset, indent=4))
+        raise Exception('{} datasets for {}'.format(len(datasets), library_id))
+
+    return datasets[0]
+
+
+@click.command()
+@click.argument('search_ids_filename', required=True)
+@click.option('--tag_name', required=False)
+def find_pseudobulk_datasets(search_ids_filename, tag_name=None):
     tantalus_api = TantalusApi()
+
+    search_ids = pd.read_csv(
+        search_ids_filename, sep='\t', names=['sample_id', 'library_id'])
 
     pseudobulk_datasets = []
 
-    for sample_id in sample_ids:
-        datasets = list(tantalus_api.list(
-            'sequence_dataset',
-            sample__sample_id=sample_id,
-            dataset_type='BAM',
-            library__library_type__name='SC_WGS',
-            aligner__name='BWA_ALN_0_5_7',
-            reference_genome__name='HG19',
-        ))
+    for idx, row in search_ids.iterrows():
+        dataset = get_dataset(tantalus_api, row['sample_id'], row['library_id'])
+        print(dataset['sample']['sample_id'], dataset['library']['library_id'])
+        pseudobulk_datasets.append(dataset['id'])
 
-        datasets = filter(lambda d: d['is_complete'], datasets)
-
-        if len(datasets) == 0:
-            raise Exception('no datasets for {}'.format(sample_id))
-
-        library_ids = set([a['library']['library_id'] for a in datasets])
-
-        for library_id in library_ids:
-            library_datasets = filter(lambda d: d['library']['library_id'] == library_id, datasets)
-
-            if len(library_datasets) != 1:
-                print sample_id, library_id
-                pprint.pprint(library_datasets)
-
-            pseudobulk_datasets.append(library_datasets[0]['id'])
-
-    tantalus_api.tag(tag_name, sequencedataset_set=pseudobulk_datasets)
+    if tag_name is not None:
+        tantalus_api.tag(tag_name, sequencedataset_set=pseudobulk_datasets)
 
 
 if __name__ == '__main__':
