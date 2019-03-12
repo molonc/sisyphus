7a74bc2db26eac1e5a17bdc69397f9397feb9b8f
Jenifer Pham <jpham@bccrc.ca>
2019-03-12 23:28:44 +0000

    fixed logging

--- workflows/get_analyses.py
+++ workflows/get_analyses.py
@@ -22,8 +22,6 @@ log.setLevel(logging.DEBUG)
 stream_handler = logging.StreamHandler()
 formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 stream_handler.setFormatter(formatter)
-log.addHandler(stream_handler)
-log.propagate = False
 
 
 def get_sequencings(library_id):
