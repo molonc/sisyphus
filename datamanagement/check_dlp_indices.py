c5adcee00da081a5d970ba27575a37f919972b7b
Andrew McPherson <andrew.mcpherson@gmail.com>
2019-03-15 22:21:48 +0000

    oops

--- datamanagement/check_dlp_indices.py
+++ datamanagement/check_dlp_indices.py
@@ -46,31 +46,18 @@ def check_indices(library_id=None):
 
         for flowcell_lane in lane_datasets:
 
-            import json
             # Get tantalus sublibraries and indices
             tantalus_indices = set()
             tantalus_dataset_ids = []
             tantalus_sequencing_centre = set()
             for dataset in lane_datasets[flowcell_lane]:
-                # if dataset['id'] != 6809:
-                #     continue
-                print(dataset['sample']['sample_id'], dataset['library']['library_id'], dataset['id'])
                 file_resources = list(tantalus_api.list('file_resource', sequencedataset__id=dataset['id']))
-                print(len(file_resources))
-                print(len(set([a['sequencefileinfo']['index_sequence'] for a in file_resources])))
-                # print(json.dumps(sorted(file_resources, key=lambda a: a['id']), sort_keys=True, indent=4))
-                # print(json.dumps(sorted([a['sequencefileinfo']['index_sequence'] for a in file_resources]), sort_keys=True, indent=4))
                 tantalus_indices.update(set([a['sequencefileinfo']['index_sequence'] for a in file_resources]))
-                print(len(set(tantalus_indices)))
-                # print(json.dumps(sorted(tantalus_indices), sort_keys=True, indent=4))
                 tantalus_dataset_ids.append(dataset['id'])
                 tantalus_sequencing_centre.update([a['sequencing_centre'] for a in dataset['sequence_lanes']])
 
             assert len(tantalus_sequencing_centre) == 1
             tantalus_sequencing_centre = list(tantalus_sequencing_centre)[0]
-
-            print(len(tantalus_indices))
-            print(len(colossus_indices))
 
             if len(colossus_indices - tantalus_indices) > 0:
                 print('library {}, datasets {}, lane {}, sequencing_centre {}: {} in colossus but not tantalus'.format(
