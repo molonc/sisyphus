ca437c445d67f11d04e352343fd90326005118a1
dranew <andrew.mcpherson@gmail.com>
2019-03-29 10:41:24 -0700

    refactor: better version of remove deleted

--- datamanagement/remove_deleted.py
+++ datamanagement/remove_deleted.py
@@ -8,13 +8,18 @@ from dbclients.tantalus import TantalusApi
 @click.command()
 @click.argument('storage_name', nargs=1)
 @click.option('--dry_run', is_flag=True)
+@click.option('--check_remote')
 def main(
     storage_name,
     dry_run=False,
+    check_remote=None,
 ):
     tantalus_api = TantalusApi()
 
     storage_client = tantalus_api.get_storage_client(storage_name)
+    remote_storage_client = None
+    if check_remote is not None:
+        remote_storage_client = tantalus_api.get_storage_client(check_remote)
 
     file_instances = tantalus_api.list(
         'file_instance',
@@ -29,6 +34,21 @@ def main(
         print('deleting file instance {}'.format(file_instance['id']))
         if not dry_run:
             tantalus_api.delete('file_instance', id=file_instance['id'])
+        if remote_storage_client:
+            remote_instance = None
+            for other_instance in file_resource['file_instances']:
+                if other_instance['storage']['name'] == check_remote:
+                    remote_instance = other_instance
+            if not remote_instance:
+                print('not deleting, no other instance')
+                continue
+            if remote_instance['is_deleted']:
+                print('not deleting, other instance deleted')
+                continue
+            if not remote_storage_client.exists(file_resource['filename']):
+                print('not deleting, other instance doesnt exist')
+                continue
+            print('found other instance')
         if len(file_resource['file_instances']) == 1:
             assert file_resource['file_instances'][0]['id'] == file_instance['id']
             print('deleting file resource {}'.format(file_resource['id']))
