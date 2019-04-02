c09d4c4302449f089b05cbb16ced468732143f16
dranew <andrew.mcpherson@gmail.com>
2019-04-01 17:10:24 -0700

    update

--- datamanagement/remove_deleted.py
+++ datamanagement/remove_deleted.py
@@ -4,6 +4,7 @@ import json
 import click
 import logging
 from dbclients.tantalus import TantalusApi
+from utils.constants import LOGGING_FORMAT
 
 
 logging.basicConfig(format=LOGGING_FORMAT, stream=sys.stderr, level=logging.INFO)
@@ -31,11 +32,32 @@ def main(
         storage__name=storage_name,
         is_deleted=True)
 
+    # DEBUG: check whether we are getting back
+    # consistent ordered results from tantalus
+    file_instances = list(file_instances)
+    file_instance_ids = set([f['id'] for f in file_instances])
+    if len(file_instances) != len(file_instance_ids):
+        raise Exception('received duplicate results from tantalus')
+
+    logging.info('processing {} file instances'.format(
+        len(file_instance_ids)))
+    logging.info('processing the following file instances: {}'.format(
+        str(file_instance_ids)))
+
     for file_instance in file_instances:
         file_resource = tantalus_api.get('file_resource', id=file_instance['file_resource'])
 
         logging.info('checking file instance {}, file resource {}, filepath {}'.format(
             file_instance['id'], file_resource['id'], file_instance['filepath']))
+
+        sequencedatasets = tantalus_api.list('sequencedataset', file_resources__id=file_resource['id'])
+        resultsdatasets = tantalus_api.list('resultsdataset', file_resources__id=file_resource['id'])
+
+        sequencedataset_ids = list(set([a['id'] for a in sequencedatasets]))
+        resultsdataset_ids = list(set([a['id'] for a in resultsdatasets]))
+
+        logging.info('file resource {} belongs to sequencedataset {} and resultsdatasets {}'.format(
+            file_resource['id'], sequencedataset_ids, resultsdataset_ids))
 
         # Optionally check for a remote version
         if remote_storage_client:
