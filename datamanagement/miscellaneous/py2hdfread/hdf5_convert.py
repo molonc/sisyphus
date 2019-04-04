8421e8f2a3e5f6482b3741bdf29addd944c6ef22
Andrew McPherson <andrew.mcpherson@gmail.com>
2019-04-03 18:31:01 -0700

    SCDNA-124: updated yaml and name key map

--- datamanagement/miscellaneous/py2hdfread/hdf5_convert.py
+++ datamanagement/miscellaneous/py2hdfread/hdf5_convert.py
@@ -13,25 +13,33 @@ pandas_to_std_types = {
 }
 
 
-def write_csv_with_types(data, filename):
+def write_csv_with_types(data, filename, header=True):
     """ Write data frame to csv with types in accompanying yaml.
     Args:
-        data: DataFrame
-        filename: csv filename ends with '.csv' or '.csv.gz'
+        data (DataFrame): data to serialize
+        filename (str): gzipped csv filename
+
+    KwArgs:
+        header (boolean): write header into csv
     """
 
     if len(data.columns) != len(data.columns.unique()):
         raise ValueError('duplicate columns not supported')
 
-    data.to_csv(filename, compression='gzip', index=False)
+    data.to_csv(filename, compression='gzip', index=False, header=header)
 
-    typeinfo = {}
+    metadata = {}
+    metadata['header'] = header
+    metadata['columns'] = []
     for column, dtype in data.dtypes.iteritems():
-        typeinfo[column] = pandas_to_std_types[str(dtype)]
+        metadata['columns'].append({
+            'name': column,
+            'dtype': pandas_to_std_types[str(dtype)],
+        })
 
     yaml_filename = filename + '.yaml'
     with open(yaml_filename, 'w') as f:
-        yaml.dump(typeinfo, f, default_flow_style=False)
+        yaml.dump(metadata, f, default_flow_style=False)
 
 
 @click.group()
@@ -66,7 +74,41 @@ h5_key_name_map = {
         '/snv/snpeff': '_snv_snpeff',
         '/snv/tri_nucleotide_context': '_snv_trinuc',
         '/strelka/vcf': '_snv_strelka',
-    }
+    },
+    '_alignment_metrics.h5': {
+        '/alignment/metrics': '_alignment_metrics',
+        '/alignment/gc_metrics': '_gc_metrics',
+    },
+    '_hmmcopy.h5': {
+        '/hmmcopy/segments/0': '_multiplier0_segments',
+        '/hmmcopy/segments/1': '_multiplier1_segments',
+        '/hmmcopy/segments/2': '_multiplier2_segments',
+        '/hmmcopy/segments/3': '_multiplier3_segments',
+        '/hmmcopy/segments/4': '_multiplier4_segments',
+        '/hmmcopy/segments/5': '_multiplier5_segments',
+        '/hmmcopy/segments/6': '_multiplier6_segments',
+        '/hmmcopy/reads/0': '_multiplier0_reads',
+        '/hmmcopy/reads/1': '_multiplier1_reads',
+        '/hmmcopy/reads/2': '_multiplier2_reads',
+        '/hmmcopy/reads/3': '_multiplier3_reads',
+        '/hmmcopy/reads/4': '_multiplier4_reads',
+        '/hmmcopy/reads/5': '_multiplier5_reads',
+        '/hmmcopy/reads/6': '_multiplier6_reads',
+        '/hmmcopy/params/0': '_multiplier0_params',
+        '/hmmcopy/params/1': '_multiplier1_params',
+        '/hmmcopy/params/2': '_multiplier2_params',
+        '/hmmcopy/params/3': '_multiplier3_params',
+        '/hmmcopy/params/4': '_multiplier4_params',
+        '/hmmcopy/params/5': '_multiplier5_params',
+        '/hmmcopy/params/6': '_multiplier6_params',
+        '/hmmcopy/metrics/0': '_multiplier0_metrics',
+        '/hmmcopy/metrics/1': '_multiplier1_metrics',
+        '/hmmcopy/metrics/2': '_multiplier2_metrics',
+        '/hmmcopy/metrics/3': '_multiplier3_metrics',
+        '/hmmcopy/metrics/4': '_multiplier4_metrics',
+        '/hmmcopy/metrics/5': '_multiplier5_metrics',
+        '/hmmcopy/metrics/6': '_multiplier6_metrics',
+    },
 }
 
 
