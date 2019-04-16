7aaeb4ee954ffea97c0243b9e9d71638c08ebe02
Michael Yuen <miyuen@momac27.bccrc.ca>
2019-04-16 14:05:12 -0700

    formatted according to black protocol

--- datamanagement/fixups/fix_csv_yaml_bool.py
+++ datamanagement/fixups/fix_csv_yaml_bool.py
@@ -13,50 +13,44 @@ from dbclients.tantalus import TantalusApi
 from dbclients.tantalus import BlobStorageClient
 
 remote_storage_name = "singlecellblob_results"
-PATTERN='.csv.gz.yaml'
+PATTERN = ".csv.gz.yaml"
+
 
 @click.command()
-@click.option('--dry_run', is_flag=True)
+@click.option("--dry_run", is_flag=True)
 def edit_yaml(dry_run=False):
 
-    file_resources = tantalus_api.list(
-        'file_resource',
-        filename__endswith=PATTERN,
-    )
+    file_resources = tantalus_api.list("file_resource", filename__endswith=PATTERN)
 
-    with open("format_cleanup_req.txt", "w") as f_out:
-        for file_resource in file_resources:
-            edited=False
-            f_path=os.path.join("/singlecelldata/results", file_resource['filename'])
+    for file_resource in file_resources:
+        stream = io.BytesIO()
+        edited = False
+        f_path = os.path.join("/singlecelldata/results", file_resource["filename"])
+        file_instance = tantalus_api.get_file_instance(
+            file_resource, remote_storage_name
+        )
+        yaml_file = blob_storage_client.open_file(file_resource["filename"])
+        file = yaml.safe_load(yaml_file)
 
-            logging.info("Analyzing {} for proper boolean format".format(f_path))
-            file_instance = tantalus_api.get_file_instance(file_resource, remote_storage_name)       
-            yaml_file=blob_storage_client.open_file(file_resource['filename'])
-            file=yaml.safe_load(yaml_file)
-            
-            if file.keys()!=['header', 'columns']:
-                logging.info("Error in {}".format(f_path))
-                f_out.write(f_path)
-                f_out.write('\n')
-                continue
-            for dtype in file['columns']:
-                if dtype['dtype']=='boolean':
-                    dtype['dtype']='bool'
-                    edited=True
-            if (not dry_run) and edited:
-                stream.write(yaml.dump(file, default_flow_style=False))
-                blob_storage_client.write_data(file_resource['filename'], stream)
-                tantalus_api.update_file(file_instance)
+        if file.keys() != ["header", "columns"]:
+            logging.info("Formatting error: {}".format(f_path))
+            continue
+        for dtype in file["columns"]:
+            if dtype["dtype"] == "boolean":
+                dtype["dtype"] = "bool"
+                edited = True
+        if (not dry_run) and edited:
+            stream.write(yaml.dump(file, default_flow_style=False))
+            blob_storage_client.write_data(file_resource["filename"], stream)
+            tantalus_api.update_file(file_instance)
 
 
 if __name__ == "__main__":
     logging.basicConfig(format=LOGGING_FORMAT, level=logging.INFO)
 
     tantalus_api = TantalusApi()
-    blob_storage_client=BlobStorageClient("singlecelldata", "results", "singlecelldata/results") 
-    stream = io.BytesIO()
+    blob_storage_client = BlobStorageClient(
+        "singlecelldata", "results", "singlecelldata/results"
+    )
 
     edit_yaml()
-
-
-
