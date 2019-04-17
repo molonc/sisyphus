e1f7423ec6e3c6c959023c834bebe916af5af212
Michael Yuen <miyuen@momac27.bccrc.ca>
2019-04-17 16:59:23 -0700

    fixed error where sentinel is written and the script crashes. The written sentinel will fool the code into not checking again when it should.

--- datamanagement/fixups/fix_csv_yaml_bool.py
+++ datamanagement/fixups/fix_csv_yaml_bool.py
@@ -34,28 +34,26 @@ def edit_yaml(dry_run=False):
         if os.path.exists(sentinel_file):
             continue
         else:
+            file_instance = tantalus_api.get_file_instance(
+                file_resource, remote_storage_name
+            )
+            yaml_file = blob_storage_client.open_file(file_resource["filename"])
+            file = yaml.safe_load(yaml_file)
+
+            if file.keys() != ["header", "columns"]:
+                logging.info("Formatting error: {}".format(f_path))
+                continue
+            for dtype in file["columns"]:
+                if dtype["dtype"] == "boolean":
+                    dtype["dtype"] = "bool"
+                    edited = True
+            if (not dry_run) and edited:
+                stream.write(yaml.dump(file, default_flow_style=False))
+                blob_storage_client.write_data(file_resource["filename"], stream)
+                tantalus_api.update_file(file_instance)
+                #exit()
             with open(sentinel_file, "w"):
                 pass
-
-        file_instance = tantalus_api.get_file_instance(
-            file_resource, remote_storage_name
-        )
-        yaml_file = blob_storage_client.open_file(file_resource["filename"])
-        file = yaml.safe_load(yaml_file)
-
-        if file.keys() != ["header", "columns"]:
-            logging.info("Formatting error: {}".format(f_path))
-            continue
-        for dtype in file["columns"]:
-            if dtype["dtype"] == "boolean":
-                dtype["dtype"] = "bool"
-                edited = True
-        if (not dry_run) and edited:
-            stream.write(yaml.dump(file, default_flow_style=False))
-            blob_storage_client.write_data(file_resource["filename"], stream)
-            tantalus_api.update_file(file_instance)
-            #exit()
-
 
 if __name__ == "__main__":
     logging.basicConfig(format=LOGGING_FORMAT, level=logging.INFO)
