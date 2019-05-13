e8e976f9f16ecbd2e07501cfdc3a90a83125826d
jeniferpham <38869713+jeniferpham@users.noreply.github.com>
2019-05-13 09:44:49 -0700

    SCDNA-209: Select reference genome from taxnomy id  (#68)
    * feat: get reference genome from taxonomy id
    
    * minor: only print scpipeline log when not using saltant
    
    * feat: function to get ref genomes
    
    * comments

---
+++ workflows/utils/colossus_utils.py
@@ -0,0 +1,26 @@
+import os
+from dbclients.colossus import ColossusApi
+
+colossus_api = ColossusApi()
+
+
+def get_ref_genome(library_info):
+	"""
+	Get reference genome from taxonomy id
+
+	Args:
+		library_info (dict): Library from colossus
+
+	Return:
+		reference_genome (str)
+	"""
+
+	taxonomy_id_map = {
+		'9606':      'HG19',
+		'10090':     'MM10',
+	}
+
+	taxonomy_id = library_info['sample']['taxonomy_id']
+	reference_genome = taxonomy_id_map[taxonomy_id]
+
+	return reference_genome
