5d4989b8698dd41e6e11a43837f8e51e01dd1dfe
Andrew McPherson <andrew.mcpherson@gmail.com>
2019-11-15 21:41:28 -0500

    feature: sublibraries keyed by cell or index sequence

--- dbclients/colossus.py
+++ dbclients/colossus.py
@@ -43,6 +43,35 @@ class ColossusApi(BasicAPIClient):
 
         return self.get("library", pool_id=library_id)
 
+    def get_sublibraries_by_field(self, library_id, field_name):
+        """ Get sublibraries in a dictionary keyed by a given field.
+        """
+        sublibrary_list = self.list("sublibraries", library__pool_id=library_id)
+
+        sublibrary_dict = dict()
+        for sublibrary in sublibrary_list:
+            if 'index_sequence' not in sublibrary:
+                sublibrary["index_sequence"] = f"{sublibrary['primer_i7']}-{sublibrary['primer_i5']}"
+
+            field_value = sublibrary[field_name]
+
+            if field_value in sublibrary_dict:
+                raise Exception(f"multiple sublibraries for {field_name} {field_value}")
+
+            sublibrary_dict[field_value] = sublibrary
+
+        return sublibrary_dict
+
+    def get_sublibraries_by_cell_id(self, library_id):
+        """ Get sublibraries in a dictionary keyed by cell_id.
+        """
+        return self.get_sublibraries_by_field(library_id, 'cell_id')
+
+    def get_sublibraries_by_index_sequence(self, library_id):
+        """ Get sublibraries in a dictionary keyed by index_sequence.
+        """
+        return self.get_sublibraries_by_field(library_id, 'index_sequence')
+
 
 _default_client = ColossusApi()
 get_colossus_sublibraries_from_library_id = (
