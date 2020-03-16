6ee69c20122f4015a4302da5db7987bbff09cf42
Andrew McPherson <andrew.mcpherson@gmail.com>
2020-03-16 17:14:25 -0400

    major rework

--- datamanagement/fixups/add_spectrum_fastq_metadata.py
+++ datamanagement/fixups/add_spectrum_fastq_metadata.py
@@ -1,209 +1,162 @@
-from dbclients.tantalus import TantalusApi
-from dbclients.colossus import ColossusApi
-from workflows import generate_inputs
-from datetime import datetime
 import yaml
 import os
 import json
+import click
+
+from dbclients.tantalus import TantalusApi
+from dbclients.colossus import ColossusApi
+from workflows import generate_inputs
 import dbclients.tantalus
+from datamanagement.utils.constants import LOGGING_FORMAT
 
 
-tantalus_api = TantalusApi()
-colossus_api = ColossusApi()
+DATASET_TYPE = 'dlpfastqs'
+DATASET_VERSION = 'v.0.0.1'
 
 
-def define_metadata_yaml():
-    return {
-        'filenames': [],
-        'meta': {
-            'type': '',
-            'version': '',
-            'cell_ids': set(),  # converted to list once populated
-            'lane_ids': set(),  # converted to list once populated
-            'sequencing_centre': '',
-            'sequencing_instrument': '',
-            'fastqs': {
-                'template': '',
-                'instances': []
+def create_lane_fastq_metadata(tantalus_api, library_id):
+    """
+    Get meatadata per lane of sequencing for a given library.
+    """
+    colossus_api = ColossusApi()
+
+    sample_info = generate_inputs.generate_sample_info(library_id)
+    index_sequence_cell_id = sample_info.set_index('index_sequence')['cell_id'].to_dict()
+
+    datasets = list(tantalus_api.list(
+        "sequencedataset", dataset_type='FQ', library__library_id=library_id))
+
+    datasets_by_lane = collections.defaultdict(list)
+
+    for dataset in datasets:
+        assert len(dataset['sequence_lanes']) == 1
+        flowcell_id = dataset['sequence_lanes'][0]['flowcell_id']
+        lane_number = dataset['sequence_lanes'][0]['lane_number']
+        datasets_by_lane[(flowcell_id, lane_number)].append(dataset)
+
+
+    for (flowcell_id, lane_number), lane_datasets in datasets_by_lane.items():
+        metadata = {'files': {}, 'meta': {}}
+
+        metadata['meta']['type'] = DATASET_TYPE
+        metadata['meta']['version'] = DATASET_VERSION
+
+        dataset_ids = set()
+        base_dirs = set()
+        sequence_lane_ids = set()
+        for dataset in lane_datasets:
+            file_resources = list(tantalus_api.list('file_resource', sequencedataset__id=dataset['id']))
+
+            dataset_ids.add(dataset['id'])
+            sequence_lane_ids.add(dataset['sequence_lanes'][0]['id'])
+
+            for file_resource in file_resources:
+                filename = os.path.basename(file_resource['filename'])
+
+                # Find common directory as subdirectory ending with flowcell/lane
+                flowcell_lane = f'{flowcell_id}_{lane_number}'
+                flowcell_idx = file_resource['filename'].index(flowcell_lane + '/')
+                flowcell_idx += len(flowcell_lane)
+                base_dir = file_resource['filename'][:flowcell_idx]
+                filename = file_resource['filename'][flowcell_idx+1:]
+                base_dirs.add(base_dir)
+
+                index_sequence = file_resource['sequencefileinfo']['index_sequence']
+                cell_id = index_sequence_cell_id[index_sequence]
+                read_end = file_resource['sequencefileinfo']['read_end']
+
+                if filename in metadata:
+                    raise ValueError(f'duplicate filename {filename}')
+
+                metadata['files'][filename] = {
+                    'cell_id': cell_id,
+                    'read_end': read_end,
+                    'flowcell_id': flowcell_id,
+                    'lane_number': lane_number,
+                }
+
+        if len(base_dirs) != 1:
+            raise ValueError(f'found files in zero or multiple directories {base_dirs}')
+
+        if len(sequence_lane_ids) != 1:
+            raise ValueError(f'found zero or multiple lanes {sequence_lane_ids}')
+
+        assert not sample_info['cell_id'].duplicated().any()
+
+        metadata['meta']['cells'] = {}
+        for idx, row in sample_info.iterrows():
+            metadata['meta']['cells'][row['cell_id']] = {
+                'library_id': row['library_id'],
+                'sample_id': row['sample_id'],
+                'pick_met': row['pick_met'],
+                'condition': row['condition'],
+                'sample_type': row['sample_type'],
+                'img_col': row['img_col'],
+                'row': row['row'],
+                'column': row['column'],
+                'primer_i5': row['primer_i5'],
+                'index_i5': row['index_i5'],
+                'primer_i7': row['primer_i7'],
+                'index_i7': row['index_i7'],
+                'index_sequence': row['index_sequence'],
+            }
+
+        metadata['meta']['lanes'] = {
+            flowcell_id: {
+                lane_number: {
+                    'sequencing_centre': lane_datasets[0]['sequence_lanes'][0]['sequencing_centre'],
+                    'sequencing_instrument': lane_datasets[0]['sequence_lanes'][0]['sequencing_instrument'],
+                    'sequencing_library_id': lane_datasets[0]['sequence_lanes'][0]['sequencing_library_id'],
+                    'read_type': lane_datasets[0]['sequence_lanes'][0]['read_type'],
+                }
             }
         }
-    }
+
+        dataset_info = {
+            'dataset_ids': dataset_ids,
+            'flowcell_id': flowcell_id,
+            'lane_number': lane_number,
+            'base_dir': list(base_dirs)[0],            
+        }
+
+        yield dataset_info, metadata
 
 
-def construct_instance_info(index_sequence, sample_info, filename):
-    instance_info = sample_info[sample_info['index_sequence'] == index_sequence]
-
-    return {
-        'sample_id':      instance_info['sample_id'].values[0],
-        'library_id':     instance_info['library_id'].values[0],
-        'cell_id':        instance_info['cell_id'].values[0],
-        'read_end':       int(filename[filename.rfind('_') + 1:filename.find('.')]),
-        'img_col':        int(instance_info['img_col'].values[0]),
-        'index_i5':       instance_info['index_i5'].values[0],
-        'index_i7':       instance_info['index_i7'].values[0],
-        'pick_met':       instance_info['pick_met'].values[0],
-        'primer_i5':      instance_info['primer_i5'].values[0],
-        'primer_i7':      instance_info['primer_i7'].values[0],
-        'index_sequence': instance_info['index_sequence'].values[0],
-        'row':            int(instance_info['row'].values[0]),
-        'column':         int(instance_info['column'].values[0]),
-    }
-
-
-def determine_fastq_template(filename, instance_info, sequence_lanes):
-    filename_templates = [
-        '{sample_id}_{library_id}_{index_sequence}_{read_end}.fastq.gz',
-        '{cell_id}_R{read_end}_001.fastq.gz',
-        '{flowcell_id}_{lane_number}_{index_sequence}_{read_end}.fastq.gz',
-    ]
-
-    filename_option1 = filename_templates[0] \
-        .replace('{sample_id}',      instance_info['sample_id']) \
-        .replace('{library_id}',     instance_info['library_id']) \
-        .replace('{index_sequence}', instance_info['index_sequence']) \
-        .replace('{read_end}',       str(instance_info['read_end']))
-
-    filename_option2 = filename_templates[1] \
-        .replace('{cell_id}',  instance_info['cell_id']) \
-        .replace('{read_end}', str(instance_info['read_end']))
-
-    filename_options3 = []
-    for sequence_lane in sequence_lanes:
-        filename_option3 = filename_templates[2] \
-            .replace('{flowcell_id}',    sequence_lane['flowcell_id']) \
-            .replace('{lane_number}',    sequence_lane['lane_number']) \
-            .replace('{index_sequence}', instance_info['index_sequence']) \
-            .replace('{read_end}',       str(instance_info['read_end']))
-        filename_options3.append(filename_option3)
-
-    if filename_option1 == filename:
-        return filename_templates[0]
-    elif filename_option2 == filename:
-        return filename_templates[1]
-    elif filename in filename_options3:
-        return filename_templates[2]
-    else:
-        raise Exception('Unable to determine file template')
-
-
-def construct_lane_info(lanes):
+@click.command()
+@click.argument('library_id')
+@click.argument('storage_name')
+@click.option('--dry_run', is_flag=True)
+def create_fastq_metadata_yaml(library_id, storage_name, dry_run=False):
     """
-    Creates dictionary of lane related info for metadata.
-    FQ datasets should only have one lane.
-
-    :param lanes:
-    :return:
+    Create a metadata.yaml file for a all FQ datasets for a library id.
     """
-    number_of_seq_lanes = len(lanes)
-    assert number_of_seq_lanes == 1, f"expected 1 sequence lane, but got {number_of_seq_lanes}"
+    tantalus_api = TantalusApi()
 
-    lane_info = {'lane_ids': set(), 'sequencing_centre': '', 'sequencing_instrument': ''}
-    centre_and_instrument_determined = False
-
-    for lane in lanes:
-        lane_info['lane_ids'].add(lane['flowcell_id'])
-        if not centre_and_instrument_determined:
-            lane_info['sequencing_centre']     = lane['sequencing_centre']
-            lane_info['sequencing_instrument'] = lane['sequencing_instrument']
-            centre_and_instrument_determined = True
-
-    return lane_info
-
-
-def create_fastq_dataset_metadata_yaml(library_id, storage_name, dry_run=False):
-    """
-    Create a metadata.yaml file for a all FQ datasets for a library id
-    on blob storage.
-    Assumes a dataset is its own directory.
-    :param library_id:
-    :return:
-    """
     storage = tantalus_api.get_storage(storage_name)
     client = tantalus_api.get_storage_client(storage_name)
 
-    datasets = tantalus_api.list("sequencedataset", dataset_type='FQ', library__library_id=library_id)
-    for dataset in datasets:
-        metadata = create_fastq_dataset_metadata_yaml(dataset)
+    for dataset_info, metadata in create_lane_fastq_metadata(tantalus_api, library_id):
+        metadata_filename = os.path.join(dataset_info['base_dir'], 'metadata.yaml')
+        metadata_filepath = tantalus_api.get_filepath('singlecellresults', metadata_filename)
 
-        # TODO:
-        metadata_save_path = ''
+        metadata_io = io.BytesIO()
+        metadata_io.write(yaml.dump(metadata, default_flow_style=False).encode())
 
-        if dry_run:
-            print(f"For dataset id '{dataset['id']}', the following metadata.yaml would have been prodcued:")
-            print(metadata)
-            continue
+        logging.info(f'writing metadata to file {metadata_filepath}')
+        client.write_data(metadata_filename, metadata_io)
 
-        metadata_as_bytes = yaml.dump(metadata, default_flow_style=False, sort_keys=False).encode()
-        metadata_save_path = os.path.join(metadata_save_path, 'metadata.yaml')
-        client.blob_service.create_blob_from_bytes(storage['storage_container'], metadata_save_path, metadata_as_bytes)
+        logging.info(f'adding {metadata_filepath} to tantalus')
 
+        if not dry_run:
+            file_resource, file_instance = tantalus_api.add_file('singlecellblob', metadata_filepath, update=True)
 
-def create_fastq_dataset_metadata_yaml(dataset):
-    """
-    Create metadata from a dataset
-    """
-    library_id = dataset['library']['library_id']
+            for dataset_id in dataset_info['dataset_ids']:
+                new_file_resources = set(results['file_resources'])
+                new_file_resources.add(file_resource['id'])
 
-    sample_info = generate_inputs.generate_sample_info(library_id)
-    metadata = define_metadata_yaml()
-    file_resources = tantalus_api.list('file_resource', sequencedataset__id=dataset['id'])
-
-    for file_resource in file_resources:
-        index_sequence = file_resource['sequencefileinfo']['index_sequence']
-        filename = os.path.basename(file_resource['filename'])
-        lane_info = construct_lane_info(dataset['sequence_lanes'])
-        instance_info = construct_instance_info(index_sequence, sample_info, filename)
-
-        metadata['meta']['type'] = dataset['library']['library_type']
-        metadata['meta']['version'] = 'v.0.0.1'  # hardcoded for now
-        metadata['meta']['sequencing_centre'] = lane_info['sequencing_centre']
-        metadata['meta']['sequencing_instrument'] = lane_info['sequencing_instrument']
-        metadata['meta']['fastqs']['template'] = \
-            determine_fastq_template(
-                filename,
-                instance_info,
-                dataset['sequence_lanes']
-            )
-        metadata['meta']['lane_ids'] = metadata['meta']['lane_ids'].union(lane_info['lane_ids'])
-        metadata['meta']['fastqs']['instances'].append(instance_info)
-        metadata['meta']['cell_ids'].add(instance_info['cell_id'])
-        metadata['filenames'].append(filename)
-
-        print(f"{library_id} - {dataset['id']} - {index_sequence} - {file_resource['filename']}")
-
-    metadata['meta']['lane_ids'] = list(metadata['meta']['lane_ids'])
-    metadata['meta']['cell_ids'] = list(metadata['meta']['cell_ids'])
-
-    return metadata
+                tantalus_api.update('sequencedataset', id=dataset_id, file_resources=list(new_file_resources))
 
 
 if __name__ == "__main__":
-
-    dataset = list(tantalus_api.list("sequencedataset", dataset_type='FQ', library__library_id='A96213A',
-                                     sample__sample_id='SA1090'))[0]
-    metadata = create_fastq_dataset_metadata_yaml(dataset)
-
-    with open('test.yaml', 'w') as meta_yaml:
-        yaml.safe_dump(metadata, meta_yaml, default_flow_style=False)
-
-    spectrum_libs = ['A108851A',
-                     'A108833A',
-                     'A108832A',
-                     'A108867A',
-                     'A96185B',
-                     'A96167A',
-                     'A96167B',
-                     'A96123A',
-                     'A98245B',
-                     'A98177A',
-                     'A98179A',
-                     'A98177B',
-                     'A96121B',
-                     'A96253']
-
-    storage_name = 'singlecellblob'
-
-    for library in spectrum_libs:
-        # create_fastq_metadata_yaml(library, storage_name)
-        create_fastq_metadata_yaml(library, storage_name, dry_run=True)
-
+    logging.basicConfig(format=LOGGING_FORMAT, level=logging.INFO)
+    create_fastq_metadata_yaml()
