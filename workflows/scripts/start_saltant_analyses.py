cea16eaf8fc592e3b259ead883c5a1be1e450fa7
Andrew McPherson <andrew.mcpherson@gmail.com>
2020-03-19 14:17:26 -0400

    feature: start an analysis by id or by table of tickets

--- workflows/scripts/start_saltant_analyses.py
+++ workflows/scripts/start_saltant_analyses.py
@@ -12,16 +12,46 @@ import workflows.utils.saltant_utils
 from datamanagement.utils.constants import LOGGING_FORMAT
 
 
-@click.command()
-@click.argument('analysis_table')
+def run_analysis(analysis, delay=None):
+    analysis_id = analysis['id']
+    analysis_status = analysis['status']
+    jira_ticket = analysis['jira_ticket']
+
+    if analysis_status in ('running', 'complete'):
+        logging.info(f'skipping analysis {analysis_id} with status {analysis_status}')
+        return
+
+    if delay is not None:
+        logging.info(f'sleeping for {delay}s')
+        time.sleep(delay)
+
+    logging.info(f'starting analysis {analysis_id} with status {analysis_status} from jira ticket {jira_ticket}')
+
+    workflows.utils.saltant_utils.get_or_create_task_instance(
+        'run_{}'.format(analysis_id),
+        'andrew', # TODO: cli
+        {
+            'analysis_id': analysis_id,
+            'jira': jira_ticket
+        },
+        14, # TODO: by name, cli
+        'andrew-pseudobulk',
+    ) # TODO: cli
+
+
+@click.group()
+def cli():
+    pass
+
+
+@cli.command()
+@click.argument('jira_ticket_file')
 @click.argument('analysis_type')
 @click.option('--delay', type=int)
-def start_analyses_saltant(analysis_table, analysis_type, delay=None):
-    logging.basicConfig(format=LOGGING_FORMAT, level=logging.INFO)
-
+def from_table(jira_ticket_file, analysis_type, delay=None):
     tantalus_api = dbclients.tantalus.TantalusApi()
 
-    analyses = pd.read_csv(analysis_table)
+    analyses = pd.read_csv(jira_ticket_file)
 
     for jira_ticket in analyses['jira_id'].unique():
         try:
@@ -32,30 +62,8 @@ def start_analyses_saltant(analysis_table, analysis_type, delay=None):
             continue
 
         for analysis in analyses:
-            analysis_id = analysis['id']
-            analysis_status = analysis['status']
-
             try:
-                if analysis_status in ('running', 'complete'):
-                    logging.info(f'skipping analysis {analysis_id} with status {analysis_status}')
-                    continue
-
-                if delay is not None:
-                    logging.info(f'sleeping for {delay}s')
-                    time.sleep(delay)
-
-                logging.info(f'starting analysis {analysis_id} with status {analysis_status} from jira ticket {jira_ticket}')
-
-                workflows.utils.saltant_utils.get_or_create_task_instance(
-                    'run_{}'.format(analysis_id),
-                    'andrew', # TODO: cli
-                    {
-                        'analysis_id': analysis_id,
-                        'jira': jira_ticket
-                    },
-                    14, # TODO: by name, cli
-                    'andrew-pseudobulk',
-                ) # TODO: cli
+                run_analysis(analysis, delay=delay)
 
             except KeyboardInterrupt:
                 raise
@@ -63,5 +71,18 @@ def start_analyses_saltant(analysis_table, analysis_type, delay=None):
             except:
                 logging.exception(f'start analysis failed for {analysis_id}')
 
+
+@cli.command()
+@click.argument('analysis_id', type=int)
+@click.option('--delay', type=int)
+def from_id(analysis_id, delay=None):
+    tantalus_api = dbclients.tantalus.TantalusApi()
+
+    analysis = tantalus_api.get('analysis', id=analysis_id)
+
+    run_analysis(analysis, delay=delay)
+
+
 if __name__ == '__main__':
-    start_analyses_saltant()
+    logging.basicConfig(format=LOGGING_FORMAT, level=logging.INFO)
+    cli()
