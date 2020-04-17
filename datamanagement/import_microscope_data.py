a79a59254ae775659a248024e8c59ee0ebd71a81
dranew <andrew.mcpherson@gmail.com>
2020-04-16 17:21:31 -0700

    datamanagement: import microscope data, annotate chip and all libraies

--- datamanagement/import_microscope_data.py
+++ datamanagement/import_microscope_data.py
@@ -17,11 +17,11 @@ from dbclients.colossus import ColossusApi
 from add_generic_results import add_generic_results
 
 
-def add_microscope_results(filepaths, library_id, storage_name, tag_name=None, update=False, remote_storage_name=None):
+def add_microscope_results(filepaths, chip_id, library_ids, storage_name, tag_name=None, update=False, remote_storage_name=None):
     colossus_api = ColossusApi()
     tantalus_api = TantalusApi()
 
-    results_name = 'MICROSCOPE_{}'.format(library_id)
+    results_name = 'MICROSCOPE_{}'.format(chip_id)
     results_type = 'MICROSCOPE'
     results_version = None
 
@@ -31,7 +31,7 @@ def add_microscope_results(filepaths, library_id, storage_name, tag_name=None, u
         results_name=results_name,
         results_type=results_type,
         results_version=results_version,
-        library_ids=[library_id],
+        library_ids=library_ids,
         recursive=True,
         tag_name=tag_name,
         update=update,
@@ -54,29 +54,33 @@ def glob_microscope_data(filepaths, storage_name, tag_name=None, update=False, r
 
     tantalus_api = TantalusApi()
 
-    library_paths = collections.defaultdict(set)
+    chip_paths = collections.defaultdict(set)
+    chip_libraries = collections.defaultdict(set)
 
     for filepath in filepaths:
-        match = re.match(r".*/single_cell_indexing/Microscope/(\d+)_(A\d+[A-Z]*)/", filepath)
+        match = re.match(r".*/single_cell_indexing/Microscope/(\d+)_(A\d+[A-Z]*)", filepath)
         if match is None:
             logging.warning('skipping malformed {}'.format(filepath))
             continue
 
         fields = match.groups()
         date = fields[0]
-        library_id = fields[1]
+        chip_id = fields[1]
 
-        try:
-            tantalus_api.get('dna_library', library_id=library_id)
-        except NotFoundError:
-            logging.warning('skipping file with unknown library {}'.format(filepath))
+        libraries = list(tantalus_api.list('dna_library', library_id__startswith=chip_id))
+
+        if len(libraries) == 0:
+            logging.eror('skipping file with unknown library {}'.format(filepath))
             continue
 
-        library_paths[library_id].add(filepath)
+        library_ids = set([library['library_id'] for library in libraries])
 
-    for library_id in library_paths:
+        chip_paths[chip_id].add(filepath)
+        chip_libraries[chip_id].update(library_ids)
+
+    for chip_id in chip_paths:
         add_microscope_results(
-            library_paths[library_id], library_id,
+            chip_paths[chip_id], chip_id, chip_libraries[chip_id],
             storage_name, tag_name=tag_name, update=update,
             remote_storage_name=remote_storage_name,
         )
@@ -84,23 +88,24 @@ def glob_microscope_data(filepaths, storage_name, tag_name=None, update=False, r
 
 @cli.command()
 @click.argument('filepaths', nargs=-1, required=True)
-@click.option('--library_id', required=True)
+@click.option('--chip_id', required=True)
+@click.option('--library_id', multiple=True)
 @click.option('--storage_name')
 @click.option('--tag_name')
 @click.option('--update', is_flag=True)
 @click.option('--remote_storage_name')
 def add_microscope_data(
-        filepaths, library_id, storage_name,
+        filepaths, chip_id, library_id, storage_name,
         tag_name=None, update=False,
         remote_storage_name=None):
 
     tantalus_api = TantalusApi()
 
     add_microscope_results(
-        filepaths, library_id,
-            storage_name, tag_name=tag_name, update=update,
-            remote_storage_name=remote_storage_name,
-        )
+        filepaths, chip_id, library_id,
+        storage_name, tag_name=tag_name, update=update,
+        remote_storage_name=remote_storage_name,
+    )
 
 
 if __name__=='__main__':
