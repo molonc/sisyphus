1cd89ea785c769af34c5b7dc15ab020df37da1d3
Andrew McPherson <andrew.mcpherson@gmail.com>
2020-11-05 09:46:18 -0500

    minor: metadata fastq

--- datamanagement/fixups/add_fastq_metadata.py
+++ datamanagement/fixups/add_fastq_metadata.py
@@ -32,7 +32,85 @@ def create_lane_fastq_metadata(tantalus_api, dataset_id):
     sample_info = generate_inputs.generate_sample_info(library_id)
     index_sequence_cell_id = sample_info.set_index('index_sequence')['cell_id'].to_dict()
 
-    metadata = {'files': {}, 'meta': {}}
+    datasets = list(tantalus_api.list(
+        "sequencedataset", dataset_type='FQ', library__library_id=library_id))
+
+    datasets_by_lane = collections.defaultdict(list)
+
+    for dataset in datasets:
+        assert len(dataset['sequence_lanes']) == 1
+        flowcell_id = dataset['sequence_lanes'][0]['flowcell_id']
+        lane_number = dataset['sequence_lanes'][0]['lane_number']
+        datasets_by_lane[(flowcell_id, lane_number)].append(dataset)
+
+
+    for (flowcell_id, lane_number), lane_datasets in datasets_by_lane.items():
+        metadata = {'files': {}, 'meta': {}}
+
+        metadata['meta']['type'] = DATASET_TYPE
+        metadata['meta']['version'] = DATASET_VERSION
+
+        dataset_ids = set()
+        base_dirs = set()
+        sequence_lane_ids = set()
+        for dataset in lane_datasets:
+            file_resources = list(tantalus_api.list('file_resource', sequencedataset__id=dataset['id']))
+
+            dataset_ids.add(dataset['id'])
+            sequence_lane_ids.add(dataset['sequence_lanes'][0]['id'])
+
+            for file_resource in file_resources:
+                filename = os.path.basename(file_resource['filename'])
+                if filename.endswith('metadata.yaml'):
+                    continue
+
+                # Find common directory as subdirectory ending with flowcell/lane
+                flowcell_lane = f'{flowcell_id}_{lane_number}'
+                flowcell_idx = file_resource['filename'].index(flowcell_lane + '/')
+                flowcell_idx += len(flowcell_lane)
+                base_dir = file_resource['filename'][:flowcell_idx]
+                filename = file_resource['filename'][flowcell_idx+1:]
+                base_dirs.add(base_dir)
+
+                index_sequence = file_resource['sequencefileinfo']['index_sequence']
+                cell_id = index_sequence_cell_id[index_sequence]
+                read_end = file_resource['sequencefileinfo']['read_end']
+
+                if filename in metadata:
+                    raise ValueError(f'duplicate filename {filename}')
+
+                metadata['files'][filename] = {
+                    'cell_id': cell_id,
+                    'read_end': read_end,
+                    'flowcell_id': flowcell_id,
+                    'lane_number': lane_number,
+                }
+
+        if len(base_dirs) != 1:
+            raise ValueError(f'found files in zero or multiple directories {base_dirs}')
+
+        if len(sequence_lane_ids) != 1:
+            raise ValueError(f'found zero or multiple lanes {sequence_lane_ids}')
+
+        assert not sample_info['cell_id'].duplicated().any()
+
+        metadata['meta']['cells'] = {}
+        for idx, row in sample_info.iterrows():
+            metadata['meta']['cells'][row['cell_id']] = {
+                'library_id': row['library_id'],
+                'sample_id': row['sample_id'],
+                'pick_met': row['pick_met'],
+                'condition': row['condition'],
+                'sample_type': row['sample_type'],
+                'img_col': row['img_col'],
+                'row': row['row'],
+                'column': row['column'],
+                'primer_i5': row['primer_i5'],
+                'index_i5': row['index_i5'],
+                'primer_i7': row['primer_i7'],
+                'index_i7': row['index_i7'],
+                'index_sequence': row['index_sequence'],
+            }
 
     metadata['meta']['type'] = DATASET_TYPE
     metadata['meta']['version'] = DATASET_VERSION
