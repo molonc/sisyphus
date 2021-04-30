49c39f4db554ca3efaa44bb64e4476bf5963ecfb
Daniel <sjmin@uwaterloo.ca>
2021-04-30 17:51:43 +0000

    constants updated

--- constants/workflows_constants.py
+++ constants/workflows_constants.py
@@ -2,4 +2,5 @@ DOCKER_IMAGES = {
 	'align': 'scdnaprod.azurecr.io/singlecellpipeline/single_cell_pipeline_alignment',
 	'hmmcopy': 'scdnaprod.azurecr.io/singlecellpipeline/single_cell_pipeline_hmmcopy',
 	'annotation': 'scdnaprod.azurecr.io/singlecellpipeline/single_cell_pipeline_annotation',
+        'breakpoint_calling': 'scdnaprod.azurecr.io/singlecellpipeline/single_cell_pipeline_breakpoint:v0.8.0',
 }
