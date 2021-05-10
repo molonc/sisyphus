40ef37392cf60056d70cd1906394361f30ed7c16
Prod v0.6.18 <sc618@scdna-prod-headnode.25k0bvbayiiutbkgbv42p53oyh.ux.internal.cloudapp.net>
2021-05-10 22:31:48 +0000

    added empty dockercreds and redirected config

---
+++ set-docker-credentials.sh
@@ -0,0 +1,9 @@
+AZURE_KEYVAULT_ACCOUNT=scdnaprod
+AZURE_BATCH_URL=https://scdnaprod.canadacentral.batch.azure.com
+CLIENT_ID=
+TENANT_ID=
+SECRET_KEY=
+SUBSCRIPTION_ID=
+RESOURCE_GROUP=scdna-prod
+AZURE_STORAGE_ACCOUNT=singlecellpypeliner
+
