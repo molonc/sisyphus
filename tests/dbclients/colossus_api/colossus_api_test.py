40082d19b3d5c126ee26213f2b25fd3da11fc44b
Daniel <sjmin@uwaterloo.ca>
2021-05-12 21:39:01 +0000

    more test cases

---
+++ tests/dbclients/colossus_api/colossus_api_test.py
@@ -0,0 +1,10 @@
+import unittest
+import dbclients
+from dbclients.colossus import ColossusApi
+
+class ColossusApiGetByLibraryIdTestCase(unittest.TestCase):
+	def test_foo(self):
+		pass
+
+if __name__ == '__main__':
+	unittest.main()
