aa26af8e73139b3c2ece49fe0ed1abd7348de2f5
seanbeatty <sbeatty@bccrc.ca>
2022-06-28 16:34:23 +0000

    remove Script and updated gitignore

--- scripts/restart_analysis.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-source /home/prod/miniconda3/bin/activate scpipeline
-source /home/prod/sisyphus/scripts/sourceMe
-analysis=$1
-echo $analysis
-#echo ~/saltant/singlecelllogs/pipeline/analysis_$1/hmmcopy
