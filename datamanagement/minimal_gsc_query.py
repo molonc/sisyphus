c6b516376e660f00be2a8f65f5ed9af5954b9ba3
Sean Beatty <sean.beatty@mail.mcgill.ca>
2025-02-13 12:56:47 -0800

    adding examples of minimal query

--- datamanagement/minimal_gsc_query.py
+++ datamanagement/minimal_gsc_query.py
@@ -2,9 +2,9 @@ import pprint
 from utils.gsc import get_sequencing_instrument, GSCAPI
 
 pp = pprint.PrettyPrinter(indent=4)
-pp.pprint(gsc_api.query("Merge?external_identifier=SA1040"))
+pp.pprint(gsc_api.query("Merge?external_identifier=SEQ1143_A143881A"))
 gsc_api = GSCAPI()
-pp.pprint(gsc_api.query("parent_library?library=PX2985"))
+pp.pprint(gsc_api.query("=library=PX3405"))
 pp.pprint(gsc_api.query("library?deliverable_format=BAM"))
 
 
@@ -12,7 +12,7 @@ gsc_api.query("merge?collaborator_id=81")
 bams = gsc_api.query("library?deliverable_format=BAM")
 collaborator_id
 collab81 = gsc_api.query("library?collaborator_id=81")
-metadata = gsc_api.query("Metadata/library?collaborator_id=81")
+metadata = gsc_api.query("Metadata/library=PX3411")
 bams[0]['collaborator_id']
 
 wgs = []
@@ -27,10 +27,15 @@ for seq_i in collab81:
         whole += [seq_i]
 
 whole[0]
-
+gsc_api.query(f"concat_fastq?library=PX3410")
+gsc_api.query(f"concat_fastq?name=SEQ1143_A143820B-GGTTGAAATTAACCCA")
+gsc_api.query(f"concat_fastq?external_identifier=SEQ1143_A1143_A143821A")
+gsc_api.query(f"concat_fastq?external_identifier=SEQ1143_*")
 external_identifier
 
 SA1040 = []
+
+pp.pprint(collab81[980249])
 
 for seq_i in whole:
     if seq_i['external_identifier'] == 'SA501':
